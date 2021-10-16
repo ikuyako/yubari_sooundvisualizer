b98613e1d04357573554563875b84b749da84ef0
ikuyako <yuki416oka@gmail.com>
2021-10-16 11:07:40 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,90 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    if (input.soundLevel() > 80) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.soundLevel() > 70) {
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            . # # # .
+            # # # # #
+            # # . # #
+            `)
+    } else if (input.soundLevel() > 60) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # . # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (input.soundLevel() > 50) {
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            # # . # #
+            # . # . #
+            . # # # .
+            `)
+    } else if (input.soundLevel() > 40) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . # . # .
+            . . # . .
+            `)
+    } else if (input.soundLevel() > 30) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (input.soundLevel() > 20) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    } else if (input.soundLevel() > 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (input.soundLevel() > 5) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
